921f8539615994c4e5e360bea2aa395694246846
Uday Bhatiya <udaybhatiya31@gmail.com>
2024-05-09 13:32:23 +0530

    Added login user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try {
+
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiErrors(500, "Something went wrong while generating access and refresh token")
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     // Get user's details from frontend
     // Validation - (fields not empty)
@@ -69,5 +86,50 @@ const registerUser = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser }
+const loginUser = asyncHandler( async (req, res) => {
+// Get data from req body
+// Username or email
+// Find the user
+// Access and refresh token
+// Send coockie
 
+const {username, email, password} = req.body
+
+if (!username || !email) {
+    throw new ApiErrors(400, "Username or email are required")
+}
+
+const user = User.findOne({
+    $or: [ {username}, {email}]
+})
+
+if (!user) {
+    throw new ApiErrors(404, "User does not exist")
+}
+
+const isPasswordValid = await user.isPasswordCorrect(password)
+if (!isPasswordValid) {
+    throw new ApiErrors(401,"Invalid password")
+}
+
+const {accessToken, refreshToken} = await generateAccessTokenAndRefreshToken(user._id);
+
+const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+const options = {
+    httpOnly: true,
+    secure: true
+};
+
+return res.status(200)
+.coockie("accessToekn", accessToken, options)
+.coockie("refreshToken", refreshToken, options)
+.json(
+    new ApiResponse(200, {
+        user: loggedInUser, accessToken, refreshToken
+    }, "User logged in successfully")
+)
+
+})
+
+export { registerUser, loginUser }
